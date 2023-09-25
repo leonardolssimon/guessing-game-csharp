125dd11d65c27fe2d7f8cc10f1e40bf32d5be09f
Leonardo L. S. Simon <70414563+leonardolssimon@users.noreply.github.com>
2023-09-24 22:45:00 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Random rnd = new Random();
+int num = rnd.Next(100);
+bool runGame = true;
+
+Console.WriteLine("Guess the number:");
+
+while (runGame){
+    int guessedNumber = Convert.ToInt32(Console.ReadLine());
+
+    if (guessedNumber < num){
+        Console.WriteLine("You choose a number lower than the actual number.");
+    }
+    else if (guessedNumber > num){
+        Console.WriteLine("You choose a number higher than the actual number.");
+    }
+    else {
+        Console.WriteLine("Congratulations! You nailed it!");
+        runGame = false;
+    }
+}
